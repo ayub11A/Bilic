a89ea66706462b95cb270c1ecdafbfde2bad4870
Ayub Mohamed <ayubimohamed854@gmail.com>
2025-11-25 00:30:26 +0300

    updated

--- src/components/CarsList.jsx
+++ src/components/CarsList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import CarsData from"../pages/CarsData"
-import CarCard from"../components/CarCard"
+import CarsData from "../pages/CarsData";
+import CarCard from "../components/CarCard";
+
 function CarsList() {
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -12,16 +13,18 @@ function CarsList() {
   return (
     <div className="p-4">
       {/* Search input */}
-      <input
-        type="text"
-        placeholder="Search newborn products..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="border p-2 rounded w-[800px] ml-[200px] mb-4"
-      />
+      <div className="flex justify-center mb-4">
+        <input
+          type="text"
+          placeholder="Search cars..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="border p-2 rounded w-full md:w-[80%] lg:w-full"
+        />
+      </div>
 
       {/* Products grid */}
-      <div className="flex flex-wrap justify-center">
+      <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {filteredItems.length === 0 ? (
           <p className="text-center w-full">No items found for "{searchTerm}"</p>
         ) : (
